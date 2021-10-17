4e0497f2a48e063670b20361a5eae4f6090617d5
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-18 01:25:20 +0600

    test

--- src/Postal.java
+++ src/Postal.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class Postal {
     public static void main(String[] args) {
-        int a=4, b=3;
-        int c;
-        System.out.println(c=a*b/a);
-
-
-
+        int a=5;
+        System.out.println(a);
+        int c=a;
+        System.out.println(c);
+        c=a+1;
+        System.out.println(c);
+        c=a+a+3;
+        System.out.println(c);
 
     }
 
