988c53783941799dd649ea780de61eccc9fce5ea
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-17 22:06:18 +0600

    Test
    Test

---
+++ src/Postal.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Postal {
+    public static void main(String[] args) {
+        int a=4, b=3;
+        int c;
+        System.out.println(c=a*b/a);
+
+
+
+
+    }
+
+}
