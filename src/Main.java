50fbd7bb3e2b8a29875baab4021ff5b526ca0a5d
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 16:16:48 +0600

    JavaLesson4_Switch-Case2

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Число К: ");
-        int k= scan.nextInt();
+        System.out.print("Введите число А: ");
+        double a= scan.nextDouble();
+        System.out.print("Введите число B: ");
+        double b= scan.nextDouble();
+        System.out.print("Выберите действие от 1 до 4: ");
+        int c= scan.nextInt();
 
-        switch (k){
-            case 1: System.out.println("Плохо");
-                break;
-            case 2: System.out.println("Неудовлетворительно");
-                break;
-            case 3: System.out.println("Удовлетворительно");
-                break;
-            case 4: System.out.println("Хорошо");
-                break;
-            case 5: System.out.println("Отлично");
-                break;
+
+        switch (c){
+            case 1:
+                System.out.print("Сложение: ");
+                System.out.println(a+b);
+                    break;
+            case 2:
+                System.out.print("Вычитание: ");
+                System.out.println(a-b);
+                    break;
+            case 3:
+                System.out.print("Умножение: ");
+                System.out.println(a*b);
+                    break;
+            case 4:
+                System.out.print("Деление: ");
+                System.out.println(a/b);
+                    break;
             default: System.out.println("Ошибка");
         }
 
