08428122391532c2fcda52c34d76102f1c47f9d1
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 11:07:22 +0600

    JavaLesson2_Scanner

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Число A: ");
-        double a = scan.nextDouble();
-        System.out.print("Число B: ");
-        double b = scan.nextDouble();
+        System.out.print("Введите четрыхзначное число: ");
+        int a = scan.nextInt();
+        int b = (a/1000);       //первая цифра 4-х значного числа
+        int c = ((a/100)%10);   //вторая цифра 4-х значного числа
+        int d = ((a/10)%10);         //третья цифра 4-х значного числа
+        int e = (a%10);         //четвертая цифра 4-х значного числа
 
-        double c = (a+b);
-        System.out.println("Сумма: "+Math.abs(c));
-        double d = (a-b);
-        System.out.println("Разность: "+Math.abs(d));
-        double e = (a*b);
-        System.out.println("Произведение: "+Math.abs(e));
-        double f = (a/b);
-        System.out.println("Частное: "+Math.abs(f));
+        boolean f = (a > 999 && a < 10000);
+        System.out.println("4-х значное число: "+f);
 
+        boolean g = ((b==c && b==d && b==e) || (b==e && c==d));
+        System.out.println("Проверка на читаемость L>R & L<R: "+g); //проверка на читаемость 4-х значного числа слева на право и справа на лево
 
 
     }
