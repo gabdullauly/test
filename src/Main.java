5e81cef9ea7811473a95dd81d59961015f81c6d4
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 14:50:40 +0600

    JavaLesson2_Scanner+Math

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Катет А: ");
+        double a = scan.nextDouble();
+        System.out.print("Катет B: ");
+        double b = scan.nextDouble();
+
+        double g = (Math.sqrt(Math.pow(a,2)+Math.pow(b,2)));
+        System.out.println("Гипотенуза: "+g);
+        double P = (a+b+g);
+        System.out.println("Периметр: "+P);
 
 
     }
