3696f657b160897095dc35c0c504c8861bf620db
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 21:55:11 +0600

    JavaLesson4_For1

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Выберите единицу измерения от 1 до 5: ");
-        int UnitMeas = scan.nextInt();
-        System.out.print("Введите значение для массы: ");
-        float m = scan.nextFloat();
+        System.out.print("Введите число A: ");
+        int a= scan.nextInt();
+        System.out.print("Введите число N: ");
+        int n= scan.nextInt();
+        double sum=0;
 
-
-        switch (UnitMeas){
-            case 1:
-                System.out.print("Выбрана единица измерения КИЛОГРАММ, введите значение массы:");
-                System.out.println(m);
-                break;
-            case 2:
-                System.out.print("Выбрана единица измерения МИЛЛИГРАММ, введите значение массы:");
-                System.out.print(m/1000000);
-                break;
-            case 3:
-                System.out.print("Выбрана единица измерения ГРАММ, введите значение массы:");
-                System.out.println(m/1000);
-                break;
-            case 4:
-                System.out.print("Выбрана единица измерения ЦЕНТНЕР, введите значение массы:");
-                System.out.println(m*100);
-                break;
-            case 5:
-                System.out.print("Выбрана единица измерения ТОННА, введите значение массы:");
-                System.out.println(m*1000);
-                break;
-            default:
-                System.out.println("Ошибка");
-                return;
+        for(double i=0; i<=n; i++){
+            sum=sum+Math.pow(a,i);
         }
-
-        System.out.println(" килограмм");
+        System.out.println(sum);
     }
-
 }
