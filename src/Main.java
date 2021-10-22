387339d683c170182c0cba8cc9c8bba8443d3ab4
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 12:05:14 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите четрыхзначное число: ");
-        int a = scan.nextInt();
-        int b = (a/1000);           //первая цифра 4-х значного числа
-        int c = ((a/100)%10);       //вторая цифра 4-х значного числа
-        int d = ((a/10)%10);         //третья цифра 4-х значного числа
-        int e = (a%10);             //четвертая цифра 4-х значного числа
+        
 
-        boolean f = (a > 999 && a < 10000);
-        System.out.println("4-х значное число: "+f);
-
-        boolean g = (b==e) && (c==d);
-        System.out.println("Проверка на читаемость L>R & L<R: "+g); //проверка на читаемость 4-х значного числа слева на право и справа на лево
 
     }
 }
