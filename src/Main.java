dcf26817d8abbc293fe02389e277b19dfebbbe9a
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 15:39:31 +0600

    JavaLesson3_Math2

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Число А: ");
-        double a = scan.nextDouble();
-        System.out.print("Число B: ");
-        double b = scan.nextDouble();
-        System.out.print("Число С: ");
-        double c = scan.nextDouble();
+        System.out.print("x1: ");
+        double x1 = scan.nextDouble();
+        System.out.print("y1: ");
+        double y1 = scan.nextDouble();
+        System.out.print("x2: ");
+        double x2 = scan.nextDouble();
+        System.out.print("y2: ");
+        double y2 = scan.nextDouble();
+
+        System.out.print("Расстояние: ");
+        System.out.println(Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2)));
 
 
-        System.out.print("Min: ");
-        if (a<b && a<c){
-            System.out.println(a);
-        }
-        else if (b<a && b<c){
-            System.out.println(b);
-        }
-        else{
-            System.out.println(c);
-        }
 
-        System.out.print("Max: ");
-        if (a>b && a>c){
-            System.out.println(a);
-        }
-        else if (b>a && b>c){
-            System.out.println(b);
-        }
-        else{
-            System.out.println(c);
-        }
 
 
     }
