883f6c09d9cca8b2b0bf2cd591fc2d80bb52ab16
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-22 16:00:15 +0600

    JavaLesson4_Switch-Case1

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("x1: ");
-        double x1 = scan.nextDouble();
-        System.out.print("y1: ");
-        double y1 = scan.nextDouble();
-        System.out.print("x2: ");
-        double x2 = scan.nextDouble();
-        System.out.print("y2: ");
-        double y2 = scan.nextDouble();
+        System.out.print("Число К: ");
+        int k= scan.nextInt();
 
-        System.out.print("Расстояние: ");
-        System.out.println(Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2)));
-
-
+        switch (k){
+            case 1: System.out.println("Плохо");
+                break;
+            case 2: System.out.println("Неудовлетворительно");
+                break;
+            case 3: System.out.println("Удовлетворительно");
+                break;
+            case 4: System.out.println("Хорошо");
+                break;
+            case 5: System.out.println("Отлично");
+                break;
+            default: System.out.println("Ошибка");
+        }
 
 
 
