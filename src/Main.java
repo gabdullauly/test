4e50fc9de52a93bfcb4081dd2e2dc709b8f73bb8
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-25 16:33:14 +0600

    JavaLesson4_For2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число A: ");
-        int a= scan.nextInt();
-        System.out.print("Введите число N: ");
-        int n= scan.nextInt();
-        double sum=0;
+        System.out.print("Введите число К: ");
+        int k = scan.nextInt();
+        System.out.print("Введите число N: ");  //необходимо число К вывести в консоль N-раз
+        int n = scan.nextInt();
 
-        for(double i=0; i<=n; i++){
-            sum=sum+Math.pow(a,i);
+
+        for ( int i=1; i<=n; i++){      //прописываю с 1 до N числа цикл выведения числа К
+            System.out.print(k+" ");
         }
-        System.out.println(sum);
+
+
+
+
+
+
     }
+
 }
