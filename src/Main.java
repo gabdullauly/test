45bf8675d91e8e865d8b118f4e7d81b42af3d0d4
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-25 18:44:11 +0600

    JavaLesson4_For3

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число К: ");
-        int k = scan.nextInt();
-        System.out.print("Введите число N: ");  //необходимо число К вывести в консоль N-раз
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Введите число N: ");
         int n = scan.nextInt();
 
+        double sum=0;
 
-        for ( int i=1; i<=n; i++){      //прописываю с 1 до N числа цикл выведения числа К
-            System.out.print(k+" ");
+        for (double i=1; i<=n; i++){
+        sum=sum+1/i;
+            System.out.print(i+" ");
+
         }
-
+        System.out.println();
+        System.out.print("Сумма последовательных чисел N-го числа: "+sum);
 
 
 
