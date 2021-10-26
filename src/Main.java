66c26af938fae26022b27636c2ffa3ea505a1804
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-26 14:25:47 +0600

    JavaLesson4_Arifmet2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число A: ");
+        long a = scan.nextLong();
         System.out.print("Введите число N: ");
-        int n = scan.nextInt();
+        byte n = scan.nextByte();
 
-        int k = n/3;
-        k++;
-        System.out.println("Наименьшее число К, при котором 3К>N: "+k);
+
+        double A = Math.pow(a,n);
+        System.out.println("Число А в степени N: "+A);
 
 
 
