e2b8749ec94a13b83e954670b0393fa50a591b83
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-10-29 21:59:51 +0600

    JavaLessons7_Array1

--- src/Postal.java
+++ src/Postal.java
@@ -1,17 +1,26 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Postal {
     public static void main(String[] args) {
-        int a=5;
-        System.out.println(a);
-        int c=a;
-        System.out.println(c);
-        c=a+1;
-        System.out.println(c);
-        c=a+a+3;
-        System.out.println(c);
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите размер массива N: ");
+        int n = scan.nextInt(); //7
+        int[] meaning = new int [n];
+        for (int i=0; i<n; i++){
+            meaning[i] = scan.nextInt();
+        }
+        //78 5 4 15 6 99 1
+        for (int i=0; i<n; i++){
+            System.out.print(meaning[n-i-1]+" ");
+        }
+//        //альтернативное решение2
+//        for (int i=n-1; i>=0; i--){
+//            System.out.print(meaning[i]+" ");
+//        }
+//        //альтернативное решение3
+//        n=n-1;
+//        for (int i=n; i>=0; i--){
+//            System.out.print(meaning[i]+" ");
+//        }
     }
-
 }
