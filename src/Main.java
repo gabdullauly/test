93fb098d969fb4fee64b3b9890066a1bfdef1d59
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-03 18:39:52 +0600

    JavaLessons8_Массив_задача7

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
-//1) Дан массив A размера N. Найти максимальный элемент из его элементов с нечетными номерами: A1, A3, A5, . . . .
+//Дан массив A размера N. Вывести вначале его элементы с четными номерами (в порядке возрастания номеров),
+// а затем — элементы с нечетными номерами (также в порядке возрастания номеров): A2, A4, A6, ..., A1, A3, A5, ....
+// Условный оператор не использовать.
+
 
 public class Main{
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Массив А: "); //4
+        System.out.print("Массив А: "); //5
         int n = scan.nextInt();
 
         int[] mean = new int[n];
         for (int i=0; i<n; i++){
-            mean[i] = scan.nextInt();   //3 5 6 7
+            mean[i] = scan.nextInt();// 4(0), 15(1), 10(2), 2(3), 22(4)
         }
-        System.out.print("Нечетные номера: ");
-        for (int i=0; i<n; i++){
-            if (mean[i]%2!=0){
-                System.out.print(mean[i]+" ");
-            }
+
+        System.out.print("Элементы четных индексов в порядке возрастания: ");
+        for (int i=0; i<n; i+=2){
+            System.out.print(mean[i]+" ");  //4 10 22
+        }
+        System.out.println();
+        System.out.print("Элементы нечетных индексов в порядке возрастания: ");
+        for (int i=1; i<n; i+=2){
+            System.out.print(mean[i]+" ");  //15 2
         }
     }
 }
