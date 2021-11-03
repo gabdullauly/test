4a3d421c567d18a0996ae70fe29b61ea0230c349
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-03 18:09:59 +0600

    JavaLessons8_Массив_задача1

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,23 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
-//5) Дан массив размеры N. Найти максимальное число всех элементов массива.
-=======
-//4) Дан массив размера N. Поменять местами его минимальный и максимальный элементы.
->>>>>>> 996814625612c375189e41023f863bf06fcd8bdc
+//1) Дан массив A размера N. Найти максимальный элемент из его элементов с нечетными номерами: A1, A3, A5, . . . .
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-<<<<<<< HEAD
-        System.out.print("Массив N: "); //4
-=======
-        System.out.print("Массив N: "); //7
->>>>>>> 996814625612c375189e41023f863bf06fcd8bdc
+        System.out.print("Массив А: "); //4
         int n = scan.nextInt();
 
         int[] mean = new int[n];
         for (int i=0; i<n; i++){
-<<<<<<< HEAD
-            mean[i] = scan.nextInt();   //15 3 20 44
+            mean[i] = scan.nextInt();   //3 5 6 7
         }
-        int max = 0;
+        System.out.print("Нечетные номера: ");
         for (int i=0; i<n; i++){
-            if(mean[i]>max){
-                mean[max] = mean[i];
+            if (mean[i]%2!=0){
+                System.out.print(mean[i]+" ");
             }
         }
-        System.out.print("max: "+mean[max]);
-=======
-            mean[i] = scan.nextInt();    // 2 45 9 6 12 3 84
-        }
-
-        int max = -2147483647;
-        int min = 2147483647;
-        for (int i=0; i<n; i++){
-            if (mean[i]>max){
-                max = i;        //находим наибольший элемент и запишем его индекс
-            }
-            if (mean[i]<min){
-                min = i;        //находим наименьший элемент и запишем его индекс
-            }
-        }
-
-//        System.out.println("max: "+mean[max]);
-//        System.out.println("min: "+mean[min]);
-        if(mean[max]!=mean[min]){
-            mean[max] = mean[max] + mean[min];      //84+2
-            mean[min] = mean[max] - mean[min];      //меняем местами то есть 86-2=84 - это mean[min]
-            mean[max] = mean[max] - mean[min];      //86-84=2 - это mean[max]
-        }
-//        System.out.println("max: "+mean[max]);
-//        System.out.println("min: "+mean[min]);
-
-        for (int i=0; i<n; i++){
-            System.out.print(mean[i]+" ");
-        }
-
->>>>>>> 996814625612c375189e41023f863bf06fcd8bdc
-
     }
 }
+
