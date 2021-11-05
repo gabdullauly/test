94f1bf7932e0454619cf94915eea56eed5b15465
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-05 18:39:11 +0600

    JavaLessons9_Задача4

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,31 @@
 import java.util.Scanner;
 
-//Дан целочисленный массив A размера N. Переписать в новый цело- численный массив B все четные числа из исходного массива (в том же порядке) и вывести размер полученного массива B и его содержимое.
+//4) Дан целочисленный массив размера N. Вывести вначале все содержащиеся в данном массиве четные числа в порядке
+// возрастания их индексов, а затем — все нечетные числа в порядке убывания их индексов.
 
 
 public class Main{
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Размер массива А: "); //4
+        System.out.print("Массив размера N: "); //7
         int n = scan.nextInt();
 
-        int[] a = new int[n];
-        for (int i=0; i<n; i++) {
-            a[i] = scan.nextInt();   //2 6 8 1
-        }
-
-        int k=0;    //счетчик
+        int[] mean = new int[n];
         for (int i=0; i<n; i++){
-            if (a[i]%2==0){
-                k++;
-            }
+            mean[i] = scan.nextInt();       //13 26 8 1 10 5 6
         }
-
-        System.out.print("Четные значения из массива А в массиве В: ");
-        int[] b = new int[k];
-        k=0;    //обратно задаем 0 чтобы был счетчиком
+        System.out.print("Четные числа в порядке возрастания их индексов: ");
         for (int i=0; i<n; i++){
-            if (a[i]%2==0){     //проверка на четность
-                b[k]=a[i];      // записывает число в размер к
-                System.out.print(b[k++]+" ");   //выводит последовательность четных чисел в индексе
+            if (mean[i]%2==0){
+                System.out.print(i+":"+mean[i]+" ");
             }
-
         }
         System.out.println();
-        System.out.print("Размер массива В: "+k);
-
+        System.out.print("Нечетные числа в порядке убывания их индексов: ");
+        for (int i=n-1; i>=0; i--){
+            if(mean[i]%2!=0){
+                System.out.print(i+":"+mean[i]+" ");
+            }
+        }
     }
 }
-
-
-
-
-
