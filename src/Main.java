0df5a6009612e2f282905d9215f375b55f52e033
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-05 19:50:32 +0600

    JavaLessons9_Задача2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
 
-//4) Дан целочисленный массив размера N. Вывести вначале все содержащиеся в данном массиве четные числа в порядке
-// возрастания их индексов, а затем — все нечетные числа в порядке убывания их индексов.
+//2) Дано целое число N (>2). Сформировать и вывести целочисленный массив размера N,
+// содержащий N первых элементов последовательности чисел Фибоначчи FK:  F1 =1, F2 =1, FK =FK−2 +FK−1, K =3,4,....
 
 
 public class Main{
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Массив размера N: "); //7
-        int n = scan.nextInt();
+        System.out.print("Введите число N>2: ");
+        int n = scan.nextInt(); //6
 
-        int[] mean = new int[n];
-        for (int i=0; i<n; i++){
-            mean[i] = scan.nextInt();       //13 26 8 1 10 5 6
+        int[] mean = new int[n];    //???
+        for(int i=2; i<n; i++){
+            mean[0] = 0;
+            mean[1] = 1;
+            mean[i] = mean[i-2] + mean[i-1];    //0 1 1 2 3 5
+            System.out.println(mean[i]);
         }
-        System.out.print("Четные числа в порядке возрастания их индексов: ");
-        for (int i=0; i<n; i++){
-            if (mean[i]%2==0){
-                System.out.print(i+":"+mean[i]+" ");
-            }
-        }
-        System.out.println();
-        System.out.print("Нечетные числа в порядке убывания их индексов: ");
-        for (int i=n-1; i>=0; i--){
-            if(mean[i]%2!=0){
-                System.out.print(i+":"+mean[i]+" ");
-            }
-        }
+
+
+
     }
 }
