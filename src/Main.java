ec5b8d5f0264c7006bbe6524f960abd1c0bfef67
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-10 22:32:17 +0600

    JavaLessons10_Бабл_сорт

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,26 @@
 import java.util.Scanner;
 
-//1) Дан массив A размера N. Найти максимальный элемент из его элементов с нечетными номерами: A1, A3, A5, . . . .
-
-
-public class Main{
-    public static void main(String[]args){
+public class Main {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите кол-во попыток для отгадывания рандомного числа: ");  //3
-        int a = scan.nextInt();
+        System.out.print("Введите размер массива: ");
+        int n = scan.nextInt(); //7
 
-        int rand = (int)(Math.random()*30+1);
-
-
-        for (int i=1; i<=a; i++){
-            System.out.print("Введите число от 1 до 30: ");
-            int b = scan.nextInt();
-            if (b==rand){
-                System.out.println("Бинго");
-                return;
+        int[] sc = new int[n];
+        for(int i=0; i<n; i++){
+            sc[i] = scan.nextInt();    //3 16 5 99 15 32 0
+        }
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-i-1; j++){
+                if (sc[j]<sc[j+1]){
+                    int temp = sc[j];
+                    sc[j] = sc[j+1];
+                    sc[j+1] = temp;
                 }
-                else if (b<rand){
-                System.out.println("Подсказка: нужно больше");
-                }
-                else if (b>rand){
-                System.out.println("Подсказка: нужно меньше");
-                }
-                else {
-                System.out.println("Ошибка");
             }
         }
-        System.out.println(rand);
-
-
-
+        for (int i=0; i<n; i++){
+            System.out.print(sc[i]+" ");
+        }
     }
 }
-
-
-
-
-
