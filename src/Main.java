9dfc5f3bb7d1c1b37d22a340df47b8036c1450a6
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-15 18:32:07 +0600

    JavaLessons_15.11_Ex5

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,56 @@
 import java.util.Scanner;
 
 /*
-6) Ввести размер массива N. Через консоль прочитать каждый элемент масива.
-Вывести элемент с максимальным количеством одинаковых элементов и его количество
+5) Ввести размер массива N. Через консоль прочитать каждый элемент масива.
+Отсортировать его и добавить все его нечетные числа в новый массив в отсортированном виде.
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
-        int n = scan.nextInt();         //20
+        int n = scan.nextInt();         //7
 
-        int[] sc = new int[n];
-        for (int i=0; i<sc.length; i++){
-            sc[i] = scan.nextInt();            //1 1 4 0 3 9 8 3 3 3 8 5 4 3 3 3 85 3 2 2
+        int[] a = new int[n];
+        for (int i=0; i<a.length; i++){
+            a[i] = scan.nextInt();          //16 213 51 84 9 6 22
         }
 
-        int count = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<sc.length; i++){
-            int k=1;
-            for (int j=i+1; j<sc.length; j++){
-                if (sc[i]==sc[j]){
-                    k++;
+        for (int i=0; i< a.length-1; i++){
+            for (int j=0; j< a.length-i-1; j++){
+                if (a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
-            if(k>max){
-                max = k;
-                count = sc[i];
+        }
+
+        for (int i=0; i< a.length; i++){
+            System.out.print(a[i]+" ");         //6 9 16 22 51 84 213
+        }
+
+        System.out.println();
+
+        int k=0;
+        for (int i=0; i< a.length; i++){
+            if (a[i]%2!=0){                     //находим кол-во индексов с нечетными значениями
+                k++;
+            }
+        }                   //3
+
+        int[] b = new int[k];
+        k=0;
+        for (int i=0; i< a.length; i++){
+            if (a[i]%2!=0){
+                b[k] = a[i];            //присваиваем к индексам нечетные значения
+                k = k+1;
             }
         }
-        System.out.print("Кол-во макс одинакового элемента: "+max);
-        System.out.println();
-        System.out.print("Элемент: "+count);
 
+        for (int i=0; i<b.length; i++){
+            System.out.print(b[i]+" ");         //9 51 213
+        }
     }
 }
 
