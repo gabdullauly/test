abac113ce02349256013f703f9e4648175a96aa0
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-15 20:06:00 +0600

    JavaLessons_15.11_Ex1_alt

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
 1)	Дан массив размера N. Найти два соседних элемента, сумма которых максимальна,
-и вывести эти элементы в порядке возрастания их индексов.
+и вывести эти элементы в порядке возрастания их значений, и вывести сумму значений.
  */
 
 public class Main {
@@ -13,34 +13,26 @@ public class Main {
 
         int[] mass1 = new int[n];
         for (int i=0; i<mass1.length; i++){
-            mass1[i] = scan.nextInt();          //21 6561 1665 13 7661 564 312
+            mass1[i] = scan.nextInt();          //21 6561 1663 13 7661 564 312
         }
 
         int max = Integer.MIN_VALUE;
         int ind0 = 0;
         int ind1 = 0;
-        for (int i=0; i<mass1.length; i++){
-            for (int j=i+1; j<mass1.length; j++){
-                if (mass1[i]+mass1[i+1]>max){
+        for (int i=0; i<mass1.length-1; i++){
+            if (mass1[i]+mass1[i+1]>max){
                     max = mass1[i]+mass1[i+1];
                     ind0 = mass1[i];
                     ind1 = mass1[i+1];
-                }
             }
         }
 
         int[] mass2 = {ind0, ind1};
-        for (int i=0; i<mass2.length-1; i++){
-            for (int j=0; j<mass2.length-i-1; i++){
-                if(mass2[j]>mass2[j+1]){
-                    int temp = mass2[j];
-                    mass2[j] = mass2[j+1];
-                    mass2[j+1] = temp;
-                }
-            }
+        if (mass2[0]>mass2[1]){
+            System.out.print(mass2[1]+" "+mass2[0]);
         }
-        for (int i=0; i<mass2.length; i++){
-            System.out.print(mass2[i]+" ");
+        else {
+            System.out.print(mass2[0]+" "+mass2[1]);
         }
         System.out.println();
         System.out.print(ind0+ind1);
