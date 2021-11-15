534ac6641e18b1897765cd84b5049611df247c95
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-15 19:26:15 +0600

    JavaLessons_15.11_Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,49 @@
 import java.util.Scanner;
 
 /*
-5) Ввести размер массива N. Через консоль прочитать каждый элемент масива.
-Отсортировать его и добавить все его нечетные числа в новый массив в отсортированном виде.
+1)	Дан массив размера N. Найти два соседних элемента, сумма которых максимальна,
+и вывести эти элементы в порядке возрастания их индексов.
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
-        int n = scan.nextInt();         //7
+        int n = scan.nextInt();     //7
 
-        int[] a = new int[n];
-        for (int i=0; i<a.length; i++){
-            a[i] = scan.nextInt();          //16 213 51 84 9 6 22
+        int[] mass1 = new int[n];
+        for (int i=0; i<mass1.length; i++){
+            mass1[i] = scan.nextInt();          //21 6561 621 13 7661 564 312
         }
 
-        for (int i=0; i< a.length-1; i++){
-            for (int j=0; j< a.length-i-1; j++){
-                if (a[j]>a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+        int max = Integer.MIN_VALUE;
+        int ind0 = 0;
+        int ind1 = 0;
+        for (int i=0; i<mass1.length; i++){
+            for (int j=i+1; j<mass1.length; j++){
+                if (mass1[i]+mass1[i+1]>max){
+                    max = mass1[i]+mass1[i+1];
+                    ind0 = mass1[i];
+                    ind1 = mass1[i+1];
                 }
             }
         }
 
-        for (int i=0; i< a.length; i++){
-            System.out.print(a[i]+" ");         //6 9 16 22 51 84 213
+        int[] mass2 = {ind0, ind1};
+        for (int i=0; i<mass2.length-1; i++){
+            for (int j=0; j<mass2.length-i-1; i++){
+                if(mass2[j]>mass2[j+1]){
+                    int temp = mass2[j];
+                    mass2[j] = mass2[j+1];
+                    mass2[j+1] = temp;
+                }
+            }
         }
-
+        for (int i=0; i<mass2.length; i++){
+            System.out.print(mass2[i]+" ");
+        }
         System.out.println();
-
-        int k=0;
-        for (int i=0; i< a.length; i++){
-            if (a[i]%2!=0){                     //находим кол-во индексов с нечетными значениями
-                k++;
-            }
-        }                   //3
-
-        int[] b = new int[k];
-        k=0;
-        for (int i=0; i< a.length; i++){
-            if (a[i]%2!=0){
-                b[k] = a[i];            //присваиваем к индексам нечетные значения
-                k = k+1;
-            }
-        }
-
-        for (int i=0; i<b.length; i++){
-            System.out.print(b[i]+" ");         //9 51 213
-        }
+        System.out.print(ind0+ind1);
     }
 }
 
