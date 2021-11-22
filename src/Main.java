72123d16020b4af5c640673987d907d6b535edb6
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-22 16:16:06 +0600

    qwe1

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,32 @@
 import java.util.Scanner;
 
 /*
-3) Даны два массива A и B размера 5, элементы которых упорядочены по возрастанию.
-Объединить эти массивы так, чтобы результирующий массив C (размера 10) остался упорядоченным по возрастанию.
-( PS: В конце используете сортировку)
+3) Дана матрица размера M × N. Найти максимальный элемент
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Введите размер строки: ");
+        int n = scan.nextInt();
+        System.out.print("Введите размер столбца: ");
+        int m = scan.nextInt();
 
-        int[] massA = new int[5];
-        int[] massB = new int[5];
-
-        for (int i=0; i<massA.length; i++){
-            massA[i] = scan.nextInt();          //2 13 6 45 9
-        }
-        for (int i=0; i<massB.length; i++){
-            massB[i] = scan.nextInt();          //25 1 3 12 77
-        }
-
-        int[] massC = new int[10];
-        for (int i=0; i<massC.length-5; i++){
-            massC[i] = massA[i];
-        }
-        int k = 0;
-        for (int i=5; i<massC.length; i++){
-            massC[i] = massB[k];
-            k = k+1;
-        }
-
-        for (int i=0; i<massC.length; i++){
-            System.out.print(massC[i]+" ");
-        }
-
-        System.out.println();
-        for (int i=0; i<massC.length-1; i++){
-            for (int j=0; j< massC.length-i-1; j++){
-                if (massC[j]>massC[j+1]){
-                    int temp = massC[j];
-                    massC[j] = massC[j+1];
-                    massC[j+1] = temp;
-                }
+        int[][] sc = new int[n][m];
+        for (int i=0; i< sc.length; i++){
+            for (int j=0; j<sc[i].length; j++){
+                sc[i][j] = scan.nextInt();
             }
         }
 
-        for (int i=0; i<massC.length; i++){
-            System.out.print(massC[i]+" ");
+        for (int i=0; i<sc.length; i++){
+            for (int j=0; j<sc[i].length; j++){
+                System.out.print(sc[i][j]+" ");
+            }
+            System.out.println();
         }
+
+
+
     }
 }
-
