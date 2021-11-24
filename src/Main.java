1674b9eb8cee4e7d63e41faa15822cf2c1be51e4
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-11-25 00:53:40 +0600

    JavaLessons14_Практика_функции

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main {
+import java.util.Scanner;
 
+/*
+Напишите метод, который принимает в качестве аргумента некоторую строку и возвращает строку без гласных в ней.
+ */
+
+public class Main {
+    static String StringWithoutVowels (String a){
+        String s = "";
+        for (int i=0; i<a.length(); i++){
+            if(a.charAt(i)!='a' && a.charAt(i)!='e' && a.charAt(i)!='y' && a.charAt(i)!='u' && a.charAt(i)!='i'
+                    && a.charAt(i)!='o' && a.charAt(i)!='w'){
+                s = s+a.charAt(i);
+            }
+        }
+        return s;
+    }
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите слово: ");        //Samsung*Digital*OnePlus*iPhone
+        String n = scan.next();
+
+        System.out.println(StringWithoutVowels(n));
     }
 }
