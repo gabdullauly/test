0e8e9c9985706489dc1cd48caad26cc991783a20
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2021-12-10 18:34:34 +0600

    Урок17_ООП_Задача2

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,61 @@ a) поля fullName, age.
 б) методы move() и talk(), в которых просто вывести на консоль сообщение -"Такой-то Person говорит".
 в) Добавьте два конструктора - Person() и Person(fullName, age).
 Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age).
-
+c) создать 5 объектов и добавить в массив
+d) найти самого старшего Person из массива по возрасту(max age)
  */
 
 public class Main{
     public static void main (String[] args){
-        Person person1 = new Person("Габдуллаулы Жандос", 31);
-        Person person2 = new Person();
-        person2.fullName = "Габдулла Самал";
-        person2.age = 27;
+        Person person1 = new Person();
+        person1.fullName = "Габдуллаулы Жандос";
+        person1.age = 31;
+
+        Person person2 = new Person("Габдулла Самал", 27);
+
         System.out.println(person1.getData());
         System.out.println(person2.getData());
-        person1.getMove("Габдуллыулы Жандос");
-        System.out.println();
+
+        person1.getMove("Габдуллаулы Жандос");
         person2.getTalk("Габдулла Самал");
+
+        Person person3 = new Person("Габдулла Айсана", 4);
+        Person person4 = new Person("Габдулла Айназар", 0);
+        Person person5 = new Person("Тлепова Тумекуль", 61);
+
+        int [] arr = new int[5];
+        int maxAge = -1;
+        for (int i=0; i<5; i++){
+            arr[0] = person1.age;
+            arr[1] = person2.age;
+            arr[2] = person3.age;
+            arr[3] = person4.age;
+            arr[4] = person5.age;
+
+            if (arr[i]>maxAge){
+                maxAge = arr[i];
+            }
+        }
+
+        for (int i=0; i<5; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+        if(maxAge==person1.age){
+            System.out.println("Самый старший Person: "+person1.getData());
+        }
+        else if (maxAge==person2.age){
+            System.out.println("Самый старший Person: "+person2.getData());
+        }
+        else if (maxAge==person3.age){
+            System.out.println("Самый старший Person: "+person3.getData());
+        }
+        else if (maxAge==person4.age){
+            System.out.println("Самый старший Person: "+person4.getData());
+        }
+        else if (maxAge==person5.age){
+            System.out.println("Самый старший Person: "+person5.getData());
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,19 +1,24 @@
 public class Person {
     String fullName;
     int age;
-    public Person(){
-    }
+
+    public Person(){}
+
     public Person(String fullName, int age){
         this.fullName = fullName;
         this.age = age;
     }
+
     String getData(){
         return fullName+" "+age;
     }
+
     void getMove(String a){
-        System.out.print(a+" говорит");
+        System.out.println(a+" говорит");
     }
+
     void getTalk(String a){
-        System.out.print(a+" говорит");
+        System.out.println(a+" говорит");
     }
+
 }
